6cffee072c5ebc2b7f780049d8eec26169c90566
dralois <epple.alexander@web.de>
2017-12-04 23:49:58 +0100

    First usable level
    -Added and updated prefabs
    -Added new materials for canon and dominos
    -Added touch control (zoom, rotate, move)
    -Added click to spawn domino
    -Added click domino to make it fall over
    -Added canon ability to shoot if domino touches it
    -Improved structure and readability

---
+++ Assets/Scripts/CanonBall.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+class CanonBall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision pi_Collision)
+    {
+        //------------------------------------------------------
+        //Wenn ein Projektil mit einem Domino kollidiert
+        //------------------------------------------------------
+        if (pi_Collision.transform.tag == "Dominos")
+        {
+            //------------------------------------------------------
+            //Lösche es
+            //------------------------------------------------------
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        //------------------------------------------------------
+        //Aufräumen
+        //------------------------------------------------------
+        Destroy(gameObject);
+    }
+}
